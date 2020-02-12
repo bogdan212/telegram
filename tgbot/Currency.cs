1c1398f6d7fa840037d419acde22f64491b70e01
bogdan212 <46643880+bogdan212@users.noreply.github.com>
2020-02-12 23:37:52 +0500

    Add files via upload

---
+++ tgbot/Currency.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tgbot
+{
+    public class Currency
+    {
+        public class ApiResult
+        {
+            public Dictionary<string, Rate> Valute { get; set; }
+        }
+        public class Rate
+        {
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public float Value { get; set; }
+
+        }
+        private RestClient RC = new RestClient();
+        private const string API_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+        private ApiResult rates;
+
+
+
+        public void download()
+        {
+            var Request = new RestRequest(API_URL);
+            var Response = RC.Get(Request);
+            var json = Response.Content;
+
+            rates = JsonConvert.DeserializeObject<ApiResult>(json);
+        }
+        public string getRate(string currency)
+        {
+            if(rates.Valute.ContainsKey(currency))
+            {
+                var rate = rates.Valute[currency];
+                return $"За{rate.Nominal}{rate.Name}дают {rate.Value} рублей";
+            }
+            else
+            {
+                return $"Нет такого{currency}";
+            }
+        }
+
+
+    }
+
+
+
+
+   
+
+}
---
+++ tgbot/Forismatic.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tgbot
+{
+    public class Forismatic
+    {
+
+        public class Quote
+        {
+            public string quoteText { get; set; }
+            public string quoteAuthor { get; set; }
+        }
+
+        private RestClient RC = new RestClient();
+        private const string URL = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru";
+        public Forismatic()
+        {
+
+        }
+        public string getRandom()
+        {
+            var Request = new RestRequest(URL);
+            var Response = RC.Get(Request);
+            var json = Response.Content;
+
+            var quote = JsonConvert.DeserializeObject<Quote>(json);
+
+            if(quote.quoteAuthor.Length > 0)
+            {
+                return $" {quote.quoteAuthor} однажды сказал:{quote.quoteText}";
+
+            }
+            else
+            {
+                return quote.quoteText;
+            }
+        }
+
+
+    }
+}
